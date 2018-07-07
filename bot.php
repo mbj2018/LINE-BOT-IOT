bd552e88c4935be2225f5b5607a66677df5b1444
jeerapote <jeerapote4@hotmail.com>
2018-07-07 23:59:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,6 +23,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			echo "Heroku";
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
@@ -61,5 +62,5 @@ if (!is_null($events['events'])) {
 	}
 }
 //echo $text;
-echo "Heroku";
+
 ?>
