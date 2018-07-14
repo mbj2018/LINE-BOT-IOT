bfb1b1ae302edc02259fd68de49fa3771ea5a0a3
jeerapote <jeerapote4@hotmail.com>
2018-07-14 20:56:15 +0700

    Update Line-Lottery.php

--- Line-Lottery.php
+++ Line-Lottery.php
@@ -1,7 +1,7 @@
 <?php
 //include 'headbot.php';
 // Function Return message
-
+send_LINELottery(){
 function t1($tt1)
 {
 	$messages = [
@@ -44,5 +44,6 @@ if (!is_null($StrGet)) {
 	curl_close($ch);
 	echo $result . "\r\n";	
 	echo $StrGet;
+}
 }
 ?>
