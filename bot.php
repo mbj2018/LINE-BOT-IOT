026ce9c6aa6b95b080eb6350fb5d7b65da09d6fd
jeerapote <jeerapote4@hotmail.com>
2018-07-19 14:00:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -62,6 +62,7 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 			echo $result . "\r\n";
+			echo "TEST1";
                                     }// end if
 			
 		}
